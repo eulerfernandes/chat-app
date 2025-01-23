bb72a1f248477e9841aa2fdfa7e2067aff7fa1ab
eulerfernandes <pedraeuler@gmail.com>
2025-01-23 06:02:48 -0300

    criação de arquivo para carregar variáveis de ambiente de um arquivo .env.

---
+++ backend/src/config/env.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const env = {
+  PORT: process.env.PORT || 5000,
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_PORT: process.env.DB_PORT || "5432",
+  DB_USER: process.env.DB_USER || "postgres",
+  DB_PASS: process.env.DB_PASS || "password",
+  DB_NAME: process.env.DB_NAME || "chatdb",
+  REDIS_URL: process.env.REDIS_URL || "redis://localhost:6379",
+  ELASTICSEARCH_URL: process.env.ELASTICSEARCH_URL || "http://localhost:9200",
+  JWT_SECRET: process.env.JWT_SECRET || "supersecret",
+};
